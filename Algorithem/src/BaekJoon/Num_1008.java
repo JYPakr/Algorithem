3f10a4ac7759a5ed2313916613aa519af906c34f
3koh <3koh@LAPTOP-M5VKHVI2>
2023-07-01 20:33:40 +0900

    입출력 및 사칙연산 풀이중.

--- Algorithem/src/BaekJoon/Num_1008.java
+++ Algorithem/src/BaekJoon/Num_1008.java
@@ -1,5 +1,16 @@
 package BaekJoon;
+import java.util.Scanner;
 
 public class Num_1008 {
-
+	
+	public static void main(String[] agrs) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		double a = sc.nextDouble();
+		double b = sc.nextDouble();
+		
+		System.out.println(a / b );
+		
+	}
 }
---
+++ Algorithem/src/BaekJoon/Num_10869.java
@@ -0,0 +1,34 @@
+package BaekJoon;
+import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Num_10869 {
+
+	public static void main(String[] agrs) throws IOException {
+		
+		//첫번째 풀이
+		BufferedReader br =  new BufferedReader(new InputStreamReader(System.in));
+		
+		String lastString = "??!";
+	
+		String sId = br.readLine();
+		
+		System.out.println(sId + lastString);
+		
+		
+		//두번째 풀이
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String lastString_1 = "??!";
+		
+		String s = sc.next();
+		System.out.println(s + lastString_1);
+		
+		
+	
+	}
+}

9f4e7e7653916a3cfff9d638677ce007a92091bc
3koh <3koh@naver.com>
2023-08-11 20:11:46 +0900

    반복문 풀이중

---
+++ Algorithem/src/BaekJoon/Num_15552.java
@@ -0,0 +1,34 @@
+package BaekJoon;
+import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.io.BufferedWriter;
+import java.util.Scanner;
+
+public class Num_15552 {
+
+	public static void main(String[] agrs) throws IOException {
+		
+		//case 1
+		BufferedReader br =  new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw =  new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		
+		int T = Integer.parseInt(br.readLine());
+
+	
+		for (int i = 0; i < T; i++) {
+			st = new StringTokenizer(br.readLine());
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+			bw.write(A + B + "\n");
+		
+		}
+		br.close();
+		
+		bw.flush();
+		bw.close();
+	}
+}

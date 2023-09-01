d57a1ef2ae6e0178945ebf9013cbb9e84f62129a
3koh <3koh@naver.com>
2023-09-01 20:22:53 +0900

    심화 문제 풀이중

---
+++ Algorithem/src/BaekJoon/Num_25083.java
@@ -0,0 +1,19 @@
+package BaekJoon;
+import java.util.Scanner;
+
+public class Num_25083 {
+	
+	public static void main(String[] agrs) {
+		
+		String s = "         ,r'\"7\n" + 	
+				   "r`-_   ,'  ,/\n" + 		
+				   " \\. \". L_r'\n" + 	
+				   "   `~\\/\n" + 		
+				   "      |\n" + 		
+				   "      |";
+	
+		System.out.print(s);
+		
+		
+	}
+}

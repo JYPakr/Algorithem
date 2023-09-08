deeebc0bb4ff30dd178243fcee3ca9cb4bba4f18
JYPakr <3koh@naver.com>
2023-09-08 22:46:16 +0900

    심화 1 크로아티아 문제 풀이중

---
+++ Algorithem/src/BaekJoon/Num_2941.java
@@ -0,0 +1,27 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Num_2941 {
+	
+	public static void main(String[] agrs) throws IOException{
+		
+		//case 1
+		BufferedReader br =  new BufferedReader(new InputStreamReader(System.in));
+		
+		
+		int T = Integer.parseInt(br.readLine());
+
+	
+		for (int i = 1; i <= T; i++) {
+			
+		
+		}
+		
+		br.close();
+		
+	
+	}
+}

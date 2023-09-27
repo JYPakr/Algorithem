02c80c622413f647109af631782b35c643b101b4
3koh <3koh@naver.com>
2023-09-27 20:59:11 +0900

    백준 풀이중

---
+++ Algorithem/src/BaekJoon/Num_11047.java
@@ -0,0 +1,30 @@
+package BaekJoon;
+
+import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Stack;
+
+
+public class Num_11047 {
+	
+	public static void main(String[] agrs) throws IOException {
+		
+		//case 1
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		
+		
+		for (int i = 1; i <= N; i++) {
+			
+		}
+
+	}
+}
